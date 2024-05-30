c3fe399a3e760b2b611c6c98bef8925dbec97ffa
Salome-N <sallynjenga8@gamil.com>
2024-05-30 13:55:43 -0700

    Pascal Trianle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' Pascal`s Triangle '''
+
+def pascal_triangle(n):
+    ''' returns a list of lists of integers '''
+    if n <= 0:
+        return []
+
+    pastri = [[1]]
+        for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(pastri[i-1][j-1] + pastri[i-1][j])
+            row.append(1)
+            pastri.append(row)
+    return pastri
